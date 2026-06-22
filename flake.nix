f764a55f9675f878608c9b6fc08aedd91d10127b
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-06-22 23:59:24 +0300

    Pin Nova to fix/launcher-window-focus for window focus debugging

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
 
     nova = {
-      url = "git+ssh://git@github.com/ImperaLuna/Nova.git?ref=feat/ai-usage-bar";
+      url = "git+ssh://git@github.com/ImperaLuna/Nova.git?ref=fix/launcher-window-focus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
