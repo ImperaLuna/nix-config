50324e60416d99eaeb98d856a2fcee8c6d7d46ec
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-06-22 23:25:56 +0300

    Point nova to feat/ai-usage-bar for tray testing

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
 
     nova = {
-      url = "git+ssh://git@github.com/ImperaLuna/Nova.git";
+      url = "git+ssh://git@github.com/ImperaLuna/Nova.git?ref=feat/ai-usage-bar";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
