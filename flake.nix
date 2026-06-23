92130d015f4e270dc052cda2e7bc8f7126a9f624
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-06-23 17:23:00 +0300

    nova: track main again (stop testing fix/refactor-tray-launcher)

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
 
     nova = {
-      url = "git+ssh://git@github.com/ImperaLuna/Nova.git?ref=fix/refactor-tray-launcher";
+      url = "git+ssh://git@github.com/ImperaLuna/Nova.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
