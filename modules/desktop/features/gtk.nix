bd73a423f568816484cf9a967dab47c6ade80ca5
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-07-03 02:28:13 +0300

    Fix Nova icon theme lookup

--- modules/desktop/features/gtk.nix
+++ modules/desktop/features/gtk.nix
@@ -8,6 +8,10 @@
         name = "adw-gtk3-dark";
         package = pkgs.adw-gtk3;
       };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
       gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
       gtk4 = {
         extraConfig.gtk-application-prefer-dark-theme = 1;
@@ -18,6 +22,7 @@
     dconf.settings."org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       gtk-theme = "adw-gtk3-dark";
+      icon-theme = "Adwaita";
     };
   };
 }
--- modules/desktop/features/nova/default.nix
+++ modules/desktop/features/nova/default.nix
@@ -1,9 +1,54 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
-  flake.modules.homeManager.desktop-feature-nova = { pkgs, ... }: {
-    home.packages = [
-      inputs.nova.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
-  };
+  flake.modules.homeManager.desktop-feature-nova = { pkgs, ... }:
+    let
+      nova = inputs.nova.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+      iconThemePackages = [
+        pkgs.adwaita-icon-theme
+        pkgs.hicolor-icon-theme
+        pkgs.kdePackages.breeze-icons
+        pkgs.nixos-icons
+      ];
+
+      iconThemeDataDirs = lib.makeSearchPath "share" iconThemePackages;
+
+      novaWithIconSearchPath = pkgs.writeShellApplication {
+        name = "nova";
+        runtimeInputs = [ nova ];
+        text = ''
+          prepend_xdg_data_dir() {
+            if [ -d "$1" ]; then
+              case ":''${XDG_DATA_DIRS:-}:" in
+                *:"$1":*) ;;
+                *) export XDG_DATA_DIRS="$1''${XDG_DATA_DIRS:+:''${XDG_DATA_DIRS}}" ;;
+              esac
+            fi
+          }
+
+          prepend_xdg_data_dirs() {
+            local old_ifs="$IFS"
+            IFS=:
+            for dir in $1; do
+              prepend_xdg_data_dir "$dir"
+            done
+            IFS="$old_ifs"
+          }
+
+          prepend_xdg_data_dir "''${XDG_DATA_HOME:-$HOME/.local/share}"
+          prepend_xdg_data_dir "$HOME/.nix-profile/share"
+          prepend_xdg_data_dir "/etc/profiles/per-user/''${USER:-imperaluna}/share"
+          prepend_xdg_data_dir "/run/current-system/sw/share"
+          prepend_xdg_data_dirs "${iconThemeDataDirs}"
+
+          exec ${nova}/bin/nova "$@"
+        '';
+      };
+    in
+    {
+      home.packages = [
+        novaWithIconSearchPath
+      ];
+    };
 }
