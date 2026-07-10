fcadb3e36d79517f0d8dc1f82a3097003ae15796
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-07-10 17:10:24 +0300

    fix(niri): route screencast portal through gnome

--- modules/_systems/features/niri.nix
+++ modules/_systems/features/niri.nix
@@ -1,27 +1,13 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (_final: prev: {
-      xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (old: {
-        postInstall = (old.postInstall or "") + ''
-          substituteInPlace $out/share/xdg-desktop-portal/portals/wlr.portal \
-            --replace-fail 'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;' \
-                           'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;niri;'
-        '';
-      });
-    })
-  ];
-
   programs.niri = {
     enable = true;
     useNautilus = false;
   };
 
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
-  xdg.portal.wlr.enable = true;
   xdg.portal.config.niri = {
-    "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+    "org.freedesktop.impl.portal.ScreenCast" = "gnome";
     Screenshot = [ "gnome" ];
   };
 
