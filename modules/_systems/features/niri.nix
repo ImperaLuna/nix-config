7f3682c0817924553bc042282e8a73bfaa40783b
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-07-17 21:27:07 +0300

    Fix Discord streaming on Niri NVIDIA

--- modules/_systems/features/niri.nix
+++ modules/_systems/features/niri.nix
@@ -1,13 +1,28 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (_final: prev: {
+      xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          substituteInPlace $out/share/xdg-desktop-portal/portals/wlr.portal \
+            --replace-fail 'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;' \
+                           'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;niri;'
+        '';
+      });
+    })
+  ];
+
   programs.niri = {
     enable = true;
     useNautilus = false;
   };
 
+  # The GNOME portal cannot negotiate Discord's video format on NVIDIA.
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+  xdg.portal.wlr.enable = true;
   xdg.portal.config.niri = {
-    "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+    "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
     Screenshot = [ "gnome" ];
   };
 
