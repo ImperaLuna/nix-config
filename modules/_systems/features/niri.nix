30bbe5a5455ad534a073bf3cee80b6a0b13ad171
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-07-17 21:45:03 +0300

    Restore GNOME portal for Niri streaming

--- modules/_systems/features/niri.nix
+++ modules/_systems/features/niri.nix
@@ -1,34 +1,13 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (_final: prev: {
-      xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (old: {
-        postInstall = (old.postInstall or "") + ''
-          substituteInPlace $out/share/xdg-desktop-portal/portals/wlr.portal \
-            --replace-fail 'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;' \
-                           'UseIn=wlroots;sway;Wayfire;river;phosh;Hyprland;niri;'
-        '';
-      });
-    })
-  ];
-
   programs.niri = {
     enable = true;
     useNautilus = false;
   };
 
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
-  xdg.portal.wlr = {
-    enable = true;
-    settings.screencast = {
-      max_fps = 60;
-      # Linear DMA-BUFs avoid static frames when NVIDIA imports the capture.
-      force_mod_linear = true;
-    };
-  };
   xdg.portal.config.niri = {
-    "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+    "org.freedesktop.impl.portal.ScreenCast" = "gnome";
     Screenshot = [ "gnome" ];
   };
 
