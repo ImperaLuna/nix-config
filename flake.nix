de7d3c9376ffcd5a81e5aabf56df57fa1c709d52
ImperaLuna <rares.brezeanu.dev@gmail.com>
2026-07-29 02:17:23 +0300

    Integrate Iris into zsh workflow

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    iris = {
+      url = "github:versenilvis/iris";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixvim.url = "github:nix-community/nixvim";
 
     llm-agents.url = "github:numtide/llm-agents.nix";
--- modules/terminal/features/zsh/_module.nix
+++ modules/terminal/features/zsh/_module.nix
@@ -5,6 +5,7 @@
     ./_aliases.nix
     ./_functions.nix
     ./_highlighting.nix
+    ./_iris.nix
   ];
 
   programs.zsh.enable = true;
